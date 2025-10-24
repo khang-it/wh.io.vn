41edc1358b1b5060d7a9c032d77be788392d2305
khangle <khang.lp@suntech.com.vn>
2025-10-24 22:49:12 +0700

    fix code

---
+++ assets/ReportView-6dfnc2Ch.js
@@ -0,0 +1,2 @@
+import{t as o,s as r,c as i,a3 as l,j as p}from"./index-SMz1hUsp.js";var s=o('<div class="p-4 bg-green-100 rounded w-full h-full">📊 Report View — Report: <!></div>');function n(e){var a=s(),t=r(i(a));l(t,{}),p(e,a)}export{n as default};
+//# sourceMappingURL=ReportView-6dfnc2Ch.js.map

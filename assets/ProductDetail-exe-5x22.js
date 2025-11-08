27571079a886db353edd9630c39c330efd2fb467
Khang Le <khanglp.it@gmail.com>
2025-11-08 11:24:43 +0700

    Refactor code structure for improved readability and maintainability

--- assets/ProductDetail-exe-5x22.js
+++
@@ -1,2 +0,0 @@
-import{t as a,j as r}from"./index-SMz1hUsp.js";var e=a('<div class="p-4 bg-yellow-100 rounded w-full h-full">📦 Product Detail — SKU: <iframe src="http://localhost:8989/country-flag" width="100%" height="100%" frameborder="0" allowfullscreen=""></iframe></div>');function d(l){var t=e();r(l,t)}export{d as default};
-//# sourceMappingURL=ProductDetail-exe-5x22.js.map

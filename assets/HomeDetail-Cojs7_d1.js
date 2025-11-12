cb334a5d10018fa3eff8d310d885140ca518c6f4
Khang Le <khanglp.it@gmail.com>
2025-11-12 15:00:48 +0700

    Refactor code structure and remove redundant sections for improved readability and maintainability

---
+++ assets/HomeDetail-Cojs7_d1.js
@@ -0,0 +1,2 @@
+import{f as r,d as t}from"./index-1FR2h3gk.js";var a=r('<div class="w-full h-full"><iframe src="/timelines.html" width="100%" height="800px" style="border:none;"></iframe></div>');function o(e){var i=a();t(e,i)}export{o as default};
+//# sourceMappingURL=HomeDetail-Cojs7_d1.js.map
--- assets/ReportView-vygvBSrD.js
+++
@@ -1,2 +0,0 @@
-import{f as e,s as i,c as t,a6 as d,d as l}from"./index-DrZ5Y9xX.js";var s=e('<div class="p-4 bg-green-100 rounded w-full h-full">📊 Report View — Report: <!></div>');function f(o){var a=s(),r=i(t(a));d(r,{}),l(o,a)}export{f as default};
-//# sourceMappingURL=ReportView-vygvBSrD.js.map

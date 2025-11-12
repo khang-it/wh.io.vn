68d9c3b4ae72939f12503df9299f11ab220bb167
Khang Le <khanglp.it@gmail.com>
2025-11-12 13:20:40 +0700

    Refactor code structure for improved readability and maintainability

---
+++ assets/ProductDetail-qjlIl-ca.js
@@ -0,0 +1,2 @@
+import{f as a,d as o}from"./index-DR-ygxpZ.js";var t=a('<div class="p-4 bg-yellow-100 rounded w-full h-full">📦 Product Detail — SKU: <iframe src="http://localhost:8989/country-flag" width="100%" height="100%" frameborder="0" allowfullscreen=""></iframe></div>');function d(l){var r=t();o(l,r)}export{d as default};
+//# sourceMappingURL=ProductDetail-qjlIl-ca.js.map

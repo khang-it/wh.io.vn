19ab5c03077d7dd760526a8761d6d94fc4b5b3fc
Khang Le <khanglp.it@gmail.com>
2025-11-12 15:22:13 +0700

    🔄 Update dist from /Users/khanglp/Documents/devzone/staging/2k/www/prod on 2025-11-12 15:22:12

--- assets/ProductDetail-DNGRQ3hW.js
+++
@@ -1,2 +0,0 @@
-import{f as a,d as o}from"./index-1FR2h3gk.js";var t=a('<div class="p-4 bg-yellow-100 rounded w-full h-full">📦 Product Detail — SKU: <iframe src="http://localhost:8989/country-flag" width="100%" height="100%" frameborder="0" allowfullscreen=""></iframe></div>');function d(l){var r=t();o(l,r)}export{d as default};
-//# sourceMappingURL=ProductDetail-DNGRQ3hW.js.map
--- assets/ReportView-Chi-IXd6.js
+++
@@ -1,2 +0,0 @@
-import{f as e,s as i,c as t,a6 as d,d as l}from"./index-1FR2h3gk.js";var s=e('<div class="p-4 bg-green-100 rounded w-full h-full">📊 Report View — Report: <!></div>');function f(o){var a=s(),r=i(t(a));d(r,{}),l(o,a)}export{f as default};
-//# sourceMappingURL=ReportView-Chi-IXd6.js.map

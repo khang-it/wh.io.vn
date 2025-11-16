e0d6c883c99a7d0b4b7a8d50cf3938ba3ed885e3
Khang Le <khanglp.it@gmail.com>
2025-11-16 15:41:21 +0700

    🔄 Update dist from /Users/khanglp/Documents/devzone/staging/2k/www/prod on 2025-11-16 15:41:21

---
+++ assets/HomeDetail-DfCJL3GG.js
@@ -0,0 +1,2 @@
+import{f as r,h as t}from"./index-DolC0Kwo.js";var a=r('<div class="w-full h-full"><iframe src="/timelines.html" width="100%" height="800px" style="border:none;"></iframe></div>');function o(e){var i=a();t(e,i)}export{o as default};
+//# sourceMappingURL=HomeDetail-DfCJL3GG.js.map

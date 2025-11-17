42996a55b0bb2997a6d7249a0778b147cbd07e7e
Khang Le <khanglp.it@gmail.com>
2025-11-17 21:38:00 +0700

    🔄 Update dist from /Users/khanglp/Documents/devzone/staging/2k/www/prod on 2025-11-17 21:38:00

---
+++ assets/HomeDetail-C1v7BlnN.js
@@ -0,0 +1,2 @@
+import{f as r,h as t}from"./index-ChJTCkyc.js";var a=r('<div class="w-full h-full"><iframe src="/timelines.html" width="100%" height="800px" style="border:none;"></iframe></div>');function o(e){var i=a();t(e,i)}export{o as default};
+//# sourceMappingURL=HomeDetail-C1v7BlnN.js.map
--- assets/HomeDetail-C69WHLMW.js
+++
@@ -1,2 +0,0 @@
-import{f as r,h as t}from"./index-XlFSmMYv.js";var a=r('<div class="w-full h-full"><iframe src="/timelines.html" width="100%" height="800px" style="border:none;"></iframe></div>');function o(e){var i=a();t(e,i)}export{o as default};
-//# sourceMappingURL=HomeDetail-C69WHLMW.js.map

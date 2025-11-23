c5bca57e7736233539d9184efa6a39e62d8688b4
Khang Le <khanglp.it@gmail.com>
2025-11-23 10:59:43 +0700

    🔄 Update dist from /Users/khanglp/Documents/devzone/staging/2k/www/prod on 2025-11-23 10:59:43

---
+++ assets/HomeDetail-Ctkx62x7.js
@@ -0,0 +1,2 @@
+import{f as r,h as t}from"./index-Bz2r5G44.js";var a=r('<div class="w-full h-full"><iframe src="/timelines.html" width="100%" height="800px" style="border:none;"></iframe></div>');function o(e){var i=a();t(e,i)}export{o as default};
+//# sourceMappingURL=HomeDetail-Ctkx62x7.js.map

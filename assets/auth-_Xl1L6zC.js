53b7238bf09999db7389a17193b430f97df9bf07
Khang Le <khanglp.it@gmail.com>
2025-12-06 23:34:30 +0700

    🔄 Update dist from /Users/khanglp/Documents/devzone/staging/2k/www/prod on 2025-12-06 23:34:30

--- assets/auth-_Xl1L6zC.js
+++
@@ -1,2 +0,0 @@
-import{V as a,a5 as t,a6 as o,a7 as s}from"./index-Bq9YYCxf.js";const r=new a;async function l(){try{const e=await r.post("/auth/logout",{},{withCredentials:!0});t.set(null),o.set(null),await s(),console.log("🔐 Logged out + refresh token cleared")}catch(e){console.error("🚨 LOGOUT ERROR:",e)}}export{l};
-//# sourceMappingURL=auth-_Xl1L6zC.js.map

538d40028c9a43a7873b1067f07ed261418d2ef5
Khang Le <khanglp.it@gmail.com>
2025-12-11 08:40:07 +0700

    🔄 Update dist from /Users/khanglp/Documents/devzone/staging/2k/www/prod on 2025-12-11 08:40:07

---
+++ assets/HomeDetail-Cn8vw0DH.js
@@ -0,0 +1,2 @@
+import{f as e,h as r}from"./index-B5AibuyQ.js";var t=e("<h1>Welcome! 8.21.53</h1>");function f(o){var a=t();r(o,a)}export{f as default};
+//# sourceMappingURL=HomeDetail-Cn8vw0DH.js.map

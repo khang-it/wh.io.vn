136e08a4745a0dbc9584058b332590de8382cc94
Khang Le <khanglp.it@gmail.com>
2025-12-14 10:23:20 +0700

    🔄 Update dist from /Users/khanglp/Documents/devzone/staging/2k/www/prod on 2025-12-14 10:23:20

---
+++ assets/auth-DDLTB4Gn.js
@@ -0,0 +1,2 @@
+import{T as e,a7 as t,a8 as o,a9 as s,aa as r}from"./index-CPGsYJ72.js";const l=new e;async function c(){try{const a=await l.post("/auth/logout",{},{withCredentials:!0});t.set(null),o.set(null),await s(),await r(),console.log("🔐 Logged out + refresh token cleared")}catch(a){console.error("🚨 LOGOUT ERROR:",a)}}export{c as l};
+//# sourceMappingURL=auth-DDLTB4Gn.js.map

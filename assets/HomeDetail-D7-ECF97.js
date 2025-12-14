3a9d070b627b38c6ecc07a7ee7d79c952a240bd2
Khang Le <khanglp.it@gmail.com>
2025-12-14 12:33:13 +0700

    🔄 Update dist from /Users/khanglp/Documents/devzone/staging/2k/www/prod on 2025-12-14 12:33:13

---
+++ assets/HomeDetail-D7-ECF97.js
@@ -0,0 +1,2 @@
+import{f as e,d as r}from"./index-EWym6t5F.js";var t=e("<h1>Welcome! 8.21.53</h1>");function f(o){var a=t();r(o,a)}export{f as default};
+//# sourceMappingURL=HomeDetail-D7-ECF97.js.map
--- assets/ProductDetail-DlU7I7KO.js
+++
@@ -1,2 +0,0 @@
-import{f as a,h as o}from"./index-CPGsYJ72.js";var t=a('<div class="p-4 bg-yellow-100 rounded w-full h-full">📦 Product Detail — SKU: <iframe src="http://localhost:8989/country-flag" width="100%" height="100%" frameborder="0" allowfullscreen=""></iframe></div>');function f(l){var r=t();o(l,r)}export{f as default};
-//# sourceMappingURL=ProductDetail-DlU7I7KO.js.map

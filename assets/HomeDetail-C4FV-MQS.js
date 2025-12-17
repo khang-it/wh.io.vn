05c4356f238cd13ca505a05a99be0bb94920bd44
Khang Le <khanglp.it@gmail.com>
2025-12-17 23:18:56 +0700

    🔄 Update dist from /Users/khanglp/Documents/devzone/staging/2k/www/prod on 2025-12-17 23:18:56

---
+++ assets/HomeDetail-C4FV-MQS.js
@@ -0,0 +1,2 @@
+import{f as e,h as r}from"./index-CnSO3tLi.js";var t=e("<h1>Welcome! 8.21.53</h1>");function f(o){var a=t();r(o,a)}export{f as default};
+//# sourceMappingURL=HomeDetail-C4FV-MQS.js.map

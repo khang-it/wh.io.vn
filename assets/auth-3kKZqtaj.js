0b65f94485285247403315e2d6cac9ad793fe42e
Khang Le <khanglp.it@gmail.com>
2025-12-27 13:31:23 +0700

    🔄 Update dist from /Users/khanglp/Documents/devzone/staging/2k/www/prod on 2025-12-27 13:31:23

--- assets/auth-3kKZqtaj.js
+++
@@ -1,2 +0,0 @@
-import{H as a,a4 as t,a5 as o,a6 as s,a7 as r}from"./index-Bc68IcrN.js";const l=new a;async function c(){try{const e=await l.post("/auth/logout",{},{withCredentials:!0});t.set(null),o.set(null),await s(),await r(),console.log("🔐 Logged out + refresh token cleared")}catch(e){console.error("🚨 LOGOUT ERROR:",e)}}export{c as l};
-//# sourceMappingURL=auth-3kKZqtaj.js.map

99365d47a0c572b4e5840ab64222e6a447f3baa5
Khang Le <khanglp.it@gmail.com>
2026-01-04 13:35:41 +0700

    🔄 Update dist from /Users/khanglp/Documents/devzone/staging/2k/www/prod on 2026-01-04 13:35:41

---
+++ assets/HomeDetail-A17hgme7.js
@@ -0,0 +1,2 @@
+import{f as e,h as r}from"./index-BFiQ2yzu.js";var t=e("<h1>Welcome! 8.21.53</h1>");function f(o){var a=t();r(o,a)}export{f as default};
+//# sourceMappingURL=HomeDetail-A17hgme7.js.map

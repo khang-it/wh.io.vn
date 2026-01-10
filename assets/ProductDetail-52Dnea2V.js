a3fffc500be8bcda9e89aac8d17b3a8deac51573
Khang Le <khanglp.it@gmail.com>
2026-01-10 14:41:02 +0700

    🔄 Update dist from /Users/khanglp/Documents/devzone/staging/2k/www/prod on 2026-01-10 14:41:02

---
+++ assets/ProductDetail-52Dnea2V.js
@@ -0,0 +1,2 @@
+import{f as a,h as o}from"./index-gePAYFzA.js";var t=a('<div class="p-4 bg-yellow-100 rounded w-full h-full">📦 Product Detail — SKU: <iframe src="http://localhost:8989/country-flag" width="100%" height="100%" frameborder="0" allowfullscreen=""></iframe></div>');function f(l){var r=t();o(l,r)}export{f as default};
+//# sourceMappingURL=ProductDetail-52Dnea2V.js.map

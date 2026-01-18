0078e45a499a3edd8df871b10734a76e9f606e39
Khang Le <khanglp.it@gmail.com>
2026-01-18 15:33:14 +0700

    🔄 Update dist from /Users/khanglp/Documents/devzone/staging/2k/www/prod on 2026-01-18 15:33:14

--- assets/auth-cuv-hLR_.js
+++
@@ -1,2 +0,0 @@
-import{H as a,a6 as t,a7 as o,a8 as s,a9 as r}from"./index-CnWwVjzw.js";const l=new a;async function c(){try{const e=await l.post("/auth/logout",{},{withCredentials:!0});t.set(null),o.set(null),await s(),await r(),console.log("🔐 Logged out + refresh token cleared")}catch(e){console.error("🚨 LOGOUT ERROR:",e)}}export{c as l};
-//# sourceMappingURL=auth-cuv-hLR_.js.map

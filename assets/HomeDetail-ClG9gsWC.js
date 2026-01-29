5ce2e2472b50974dd060234bd586d24fdca9efda
Khang Le <khanglp.it@gmail.com>
2026-01-30 00:23:14 +0700

    🔄 Update dist from /Users/khanglp/Documents/devzone/staging/2k/www/prod on 2026-01-30 00:23:14

--- assets/HomeDetail-ClG9gsWC.js
+++
@@ -1,2 +0,0 @@
-import{f as e,h as r}from"./index-C3F57mgS.js";var t=e("<h1>Welcome! 8.21.53</h1>");function f(o){var a=t();r(o,a)}export{f as default};
-//# sourceMappingURL=HomeDetail-ClG9gsWC.js.map
--- assets/auth-CPJU5UFt.js
+++
@@ -1,2 +0,0 @@
-import{E as e,a8 as t,a9 as o,aa as s,ab as r}from"./index-C3F57mgS.js";const l=new e;async function c(){try{const a=await l.post("/auth/logout",{},{withCredentials:!0});t.set(null),o.set(null),await s(),await r(),console.log("🔐 Logged out + refresh token cleared")}catch(a){console.error("🚨 LOGOUT ERROR:",a)}}export{c as l};
-//# sourceMappingURL=auth-CPJU5UFt.js.map

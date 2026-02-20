68a73ff019c527baa7dfbf41325dcfd6ae2cf230
Khang Le <khanglp.it@gmail.com>
2026-02-20 15:42:34 +0700

    🔄 Update dist from /Users/khanglp/Documents/devzone/staging/2k/www/prod on 2026-02-20 15:42:34

--- assets/ProductDetail-DiokfS43.js
+++
@@ -1,2 +0,0 @@
-import{f as a,h as o}from"./index-DGYkEIRh.js";var t=a('<div class="p-4 bg-yellow-100 rounded w-full h-full">📦 Product Detail — SKU: <iframe src="http://localhost:8989/country-flag" width="100%" height="100%" frameborder="0" allowfullscreen=""></iframe></div>');function f(l){var r=t();o(l,r)}export{f as default};
-//# sourceMappingURL=ProductDetail-DiokfS43.js.map
